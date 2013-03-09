dc37edb9b50f71abeeae8d2bb639981822b3db32
Alberto Ramos <shujito.desu@gmail.com>
2013-03-09 02:56:02 -0700

    why

--- src/org/shujito/cartonbox/view/fragments/PostsFragment.java
+++ src/org/shujito/cartonbox/view/fragments/PostsFragment.java
@@ -9,7 +9,6 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.AbsListView;
 import android.widget.AbsListView.OnScrollListener;
